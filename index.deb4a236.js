2a9dff1bb8717c759eb41f17cbbbc17144a3e21c
Iryna Zahorodnia <irishas25@ukr.net>
2024-09-06 17:52:47 +0300

    make build

---
+++ index.deb4a236.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),l=0;l<e.rows.length;l++){var o=e.rows[l].cells[1].cloneNode(!0);e.rows[l].lastElementChild.before(o)}
+//# sourceMappingURL=index.deb4a236.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const table = document.querySelector("table");
+for(let i = 0; i < table.rows.length; i++){
+    const position = table.rows[i].cells[1];
+    const copyPosition = position.cloneNode(true);
+    const salary = table.rows[i].lastElementChild;
+    salary.before(copyPosition);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
